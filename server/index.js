109596b5fb8126f463fdf706dee585203b003aee
Vrushabh Gundecha <mayurshelar1098@gmail.com>
2022-11-19 23:32:51 +0530

    preparing deployment

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const connection = require("./db");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
-
+const path=require("path");
 // database connection
 connection();
 
@@ -16,6 +16,20 @@ app.use(cors());
 // routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
+
+
+__dirname = path.resolve();
+if(process.env.NODE_ENV === "production"){
+app.use(express.static(path.join(__dirname,"../client/build")));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "client","build","index.html"))
+})
+}else{
+    app.get("/",(req,res)=>{
+        res.send("API is running....");
+    })
+}
 
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
