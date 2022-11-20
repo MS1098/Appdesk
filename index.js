a4a543676a7099dc1c76838c789d478aac4f405f
Vrushabh Gundecha <mayurshelar1098@gmail.com>
2022-11-20 12:40:49 +0530

    lets deploy

---
+++ index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connection = require("./db");
+const userRoutes = require("./server/routes/users");
+const authRoutes = require("./server/routes/auth");
+const path=require("path");
+// database connection
+connection();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
+
+__dirname = path.resolve();
+if(process.env.NODE_ENV === "production"){
+app.use(express.static(path.join(__dirname,"../client/build")));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "client","build","index.html"))
+})
+}else{
+    app.get("/",(req,res)=>{
+        res.send("API is running....");
+    })
+}
+
+const port = process.env.PORT || 8080;
+app.listen(port, console.log(`Listening on port ${port}...`));
